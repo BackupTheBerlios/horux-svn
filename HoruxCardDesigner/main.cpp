f54ca37b4fc7490d14ea8f87a8e4c6e4b529b6f2
letux <letux@ec3e1dca-c368-0410-a38c-fca29f5d85da>
2009-12-29 16:57:54 +0000

    update HoruxCardDesigner

--- HoruxCardDesigner/main.cpp
+++ HoruxCardDesigner/main.cpp
@@ -1,13 +1,20 @@
 #include <QtGui/QApplication>
 #include "horuxdesigner.h"
-
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(ressource);
-
     QApplication a(argc, argv);
+
+    QPixmap pixmap(":/images/splash.jpg");
+    QSplashScreen splash(pixmap);
+    splash.show();
+
+    sleep(2);
+
     HoruxDesigner w;
+    splash.finish(&w);
     w.show();
     return a.exec();
 }
