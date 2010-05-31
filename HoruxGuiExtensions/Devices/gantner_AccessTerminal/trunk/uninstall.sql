2a5a91da82e9ca9ccd2110ae409209e4ef46d205
letux <letux@ec3e1dca-c368-0410-a38c-fca29f5d85da>
2010-05-31 11:57:56 +0000

    update gantner terminal

--- HoruxGuiExtensions/Devices/gantner_AccessTerminal/trunk/uninstall.sql
+++ HoruxGuiExtensions/Devices/gantner_AccessTerminal/trunk/uninstall.sql
@@ -1,3 +1,1 @@
-DROP TABLE IF EXISTS `hr_gantner_TimeTerminal`;
-DROP TABLE IF EXISTS `hr_gantner_standalone_action`;
-DROP TABLE IF EXISTS `hr_gantner_TimeTerminal_key`;
+DROP TABLE IF EXISTS `hr_gantner_AccessTerminal`;
