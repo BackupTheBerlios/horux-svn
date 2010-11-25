789867dd22f96dbc19235b68eaebf7cbb8dbffcb
letux <letux@ec3e1dca-c368-0410-a38c-fca29f5d85da>
2010-11-25 07:26:43 +0000

    update sql script

--- HoruxGuiExtensions/Components/velopark/trunk/install.sql
+++ HoruxGuiExtensions/Components/velopark/trunk/install.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `hr_vp_parking` (
   `device_ids` varchar(255) NOT NULL,
   `access_credit_warning_msg` varchar(255) NOT NULL,
   `access_warning_msg` varchar(255) NOT NULL,
+  `creditValue` float NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
@@ -33,11 +34,12 @@ CREATE TABLE IF NOT EXISTS `hr_vp_subscription_attribution` (
   `subcription_id` int(11) NOT NULL DEFAULT '0',
   `create_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `status` set('not_start','started','finished','waiting','canceled') NOT NULL DEFAULT 'not_start',
-  `credit` int(11) NOT NULL DEFAULT '0',
+  `credit` float NOT NULL DEFAULT '0',
   `start` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `end` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `create_by` varchar(100) NOT NULL,
   `multiticket` smallint(6) NOT NULL DEFAULT '0',
+  `serialNumber` varchar(128) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `start` (`start`,`end`,`user_id`,`subcription_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
@@ -46,6 +48,15 @@ CREATE TABLE IF NOT EXISTS `hr_vp_subscription_attribution` (
 CREATE TABLE IF NOT EXISTS `hr_vp_deleted_user` (
   `user_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `hr_vp_period` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `start` time NOT NULL,
+  `end` time NOT NULL,
+  `locked` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+)  ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO `hr_user_action` (`name`, `page`, `icon`, `tip`, `catalog`, `type`) VALUES
 ('Subscription', 'components.velopark.attribution', './protected/pages/components/velopark/assets/icon-16-ticket.jpg', 'Ticketing - Subscription Attribution', 'velopark', 'userList'),
