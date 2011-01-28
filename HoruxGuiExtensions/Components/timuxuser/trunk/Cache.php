3e14d3430c89f39839427c666930cb363468e4dd
letux <letux@ec3e1dca-c368-0410-a38c-fca29f5d85da>
2011-01-28 15:19:40 +0000

    add custome cache

---
+++ HoruxGuiExtensions/Components/timuxuser/trunk/Cache.php
@@ -0,0 +1,34 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Prado::using("System.Caching.TMemCache");
+
+class Cache extends TCache {
+
+    public $cache = array();
+
+    protected function getValue($key) {
+        return $this->cache[$key];
+    }
+
+    protected function setValue($key,$value,$expire) {
+        $this->cache[$key] = $value;
+    }
+
+    protected function addValue($key,$value,$expire) {
+        $this->cache[$key] = $value;
+    }
+
+    protected function deleteValue($key) {
+        unset($this->cache[$key]);
+    }
+
+    public function flush() {
+        $this->cache = array();
+    }
+}
+
+?>
